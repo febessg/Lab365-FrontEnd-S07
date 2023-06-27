49fb684150501021ab83688009c640a102cd9c87
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-06-26 21:54:31 -0300

    like button

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,7 +1,23 @@
 import './UserCard.css';
 import propTypes from 'prop-types';
+import heart from '../../assets/images/heart.png';
+import heartFull from '../../assets/images/heart-full.png';
+import { useState } from 'react';
+
 
 function userCard({userList}) {
+    const [like, setLike] = useState(<img className='likeImg' src={heart} alt="Like"/>)
+
+    const [totalLikes, setTotalLikes] = useState(0);
+
+    const likeUser = () => {
+        
+        setLike(<img className='likeImg' src={heartFull} alt="Like"/>)
+        setTotalLikes(totalLikes + 1)            
+      
+    }
+    
+
     return (  
         <>
             <div className="cards-h2-wrapper">
@@ -17,9 +33,10 @@ function userCard({userList}) {
                         <ul>
                         <li><strong>id:</strong> {index+1}</li>
                         <li><strong>Idade:</strong> {user.age}</li>
-                        <li><strong>E-mail:</strong> {user.email}</li>  
+                        <li><strong>E-mail:</strong> {user.email}</li>
+                        <li><strong>Likes:</strong> {totalLikes}</li>                     
                         </ul>
-                        
+                        <a onClick={likeUser}>{like}</a>
                     </div>
                 ))}
             </div>
